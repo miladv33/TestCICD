b8cd2c9be534b5a466615c3c55ff890323ad7fae
Milad Mosavi <milad.mosavi.developer@gmail.com>
2022-07-17 22:16:58 +0430

    add document to MainActivity.kt

--- app/src/main/java/com/example/testcicd/MainActivity.kt
+++ app/src/main/java/com/example/testcicd/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.testcicd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Main activity
+ *
+ * @constructor Create empty Main activity
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
